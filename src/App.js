8d598f508c8474e3562ae051286622a623da9030
Erico Bruner <ericodasilvabruner@gmail.com>
2020-12-13 02:00:55 -0300

    Criando estruturação

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Lupus from './components/index'
+import GlobalStyle from './styles/global'
+const App = () => (
+  <>
+    <Lupus />
+    <GlobalStyle />
+  </>
+)
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Title } from './styles'
+
+const Lupus = () => {
+  return <Title>Lupus</Title>
+}
+
+export default Lupus
